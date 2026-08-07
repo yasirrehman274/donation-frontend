91339d78691ed7fb4b765f22a2a6698b1ebf8b26
yasirrehman274 <yasirrehman274@gmail.com>
2026-08-08 01:32:18 +0500

    first commit

--- backend/src/middleware/index.js
+++
@@ -1,6 +0,0 @@
-const authenticate = require('./authenticate');
-const authorize = require('./authorize');
-const validate = require('./validate');
-const { notFound, errorHandler } = require('./errorHandler');
-
-module.exports = { authenticate, authorize, validate, notFound, errorHandler };
--- backend/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-const ApiError = require('./ApiError');
-const asyncHandler = require('./asyncHandler');
-const helpers = require('./helpers');
-const constants = require('./constants');
-
-module.exports = { ApiError, asyncHandler, ...helpers, ...constants };
